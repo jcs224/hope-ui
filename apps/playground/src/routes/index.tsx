147169fc7db61ffbcdc7829e46bb3bbf1e1eea8f
Fabien MARIE-LOUISE <fabienml.dev@gmail.com>
2022-11-04 08:48:10 +0100

    chore: save

--- apps/playground/src/routes/index.tsx
+++ apps/playground/src/routes/index.tsx
@@ -1,10 +1,3 @@
-import { Kbd, Button } from "@hope-ui/core";
-
 export default function Index() {
-  return (
-    <>
-      <Kbd>ctrl</Kbd>
-      <Button>Button</Button>
-    </>
-  );
+  return <></>;
 }
